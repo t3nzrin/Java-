be7686e3302fefc62170d0c8411e716298ea1cf7
Tenrin <tenrin2580@gmail.com>
2026-04-13 12:30:08 +0530

    Create singlearray.java

---
+++ singlearray.java
@@ -0,0 +1,24 @@
+//Array is a collection of elements of the same data type 
+//stored in continous memory locations.
+public class singlearray
+{
+    public static void main(String[] args)
+    {
+    //Declaring array
+    //int arr[];
+    //creation 
+    //arr = new int[5];
+    //or both
+    //int arr[] = new int[5];
+    //Declaring and initialize the array
+    int arr[]={10,20,30,40,50};
+    //Acce the array values
+    for(int i=0; i<5; i++)
+    {
+        System.out.println(arr[i]);
+
+    }
+
+    }
+
+}
