8b9abf285142748623fef40521134ecdf9ce557d
Tenrin <tenrin2580@gmail.com>
2026-04-13 12:09:24 +0530

    Create Finalvariable.java

---
+++ Finalvariable.java
@@ -0,0 +1,15 @@
+/* Final keyword is used to restrict modifications applied to variables,
+methods, and classes */
+//program for restricting modifications to variable 
+//using final keyword
+
+public class Finalvariable 
+{
+public static void main(String[] args)
+{
+final int x = 10;
+System.out.println("x=" +x);
+//x = 20;
+//System.out.println("x=" +x);
+}
+}
