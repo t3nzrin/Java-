ec4b7286f0a90a949267eece17155cea98d96c29
Tenrin <tenrin2580@gmail.com>
2026-04-13 12:36:17 +0530

    Create arrayuser.java

---
+++ arrayuser.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class arrayuser
+{
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(System.in);
+        int arr[] =  new int[3];
+        System.out.println("Enter the 3 integer data");
+        for(int i = 0; i < 3; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+    }
+}
